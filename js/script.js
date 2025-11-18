48a86b0123dc4e5ef0d6021b45360dc260fa77c6
Joselin <127jramirez@frhsd.com>
2025-11-18 13:59:26 -0500

    11/18/25

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 
 var nameBox = document.getElementById('nameInput');
-var nameAge = 
+var nameAge = document.getElementById('ageInput');
+var favColor = document.getElementById('colorInput');
+var favPet = document.getElementsByName('pet');
+var favPet = document.querySelectorAll("input[type='radio']");
+var hoBby = document.getElementsByClassName('hobby');
+var hoBy = document.querySelectorAll("input[type='checkbox']");
+var buTton = document.getElementById('submitBtn');
+var outPut = document.getElementById('output');
+const element = document.getElementById("myBtn");
+
 console.log(nameBox);
+console.log(nameAge);
+console.log(favColor);
+console.log(favPet);
+console.log(hoBby);
+console.log(buTton);
+console.log(outPut);
+console.log(textBox);
+console.log("nameInput", nameBox, "ageInput", nameAge);
