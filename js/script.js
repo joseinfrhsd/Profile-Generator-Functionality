b56acd1deeb068c3d28557b43238ea3faa1e2ce4
Joselin <127jramirez@frhsd.com>
2025-11-26 11:00:05 -0500

    11/26

--- js/script.js
+++ js/script.js
@@ -2,22 +2,37 @@
 var nameBox = document.getElementById('nameInput');
 var nameAge = document.getElementById('ageInput');
 var favColor = document.getElementById('colorInput');
-var favPet = document.getElementsByName('pet');
+var pet = document.getElementsByName('pet');
 var favPet = document.querySelectorAll("input[type='radio']");
 var hoBby = document.getElementsByClassName('hobby');
 var hoBy = document.querySelectorAll("input[type='checkbox']");
 var buTton = document.getElementById('submitBtn');
 var outPut = document.getElementById('output');
 const element = document.getElementById("myBtn");
-
+let c = [];
 buTton.addEventListener("click",info);
 function info(){
-console.log(nameBox.value);
-console.log(nameAge.value);
+    hobbies = [];
+ console.log(nameBox.value);
+ console.log(nameAge.value);
 console.log(favColor.value);
-output.innerHTML=nameBox.value +" "+ nameAge.value +" "+ favColor.value;
+for(let i = 0; i < favPet.length; i++){
+    if(favPet[i].checked){
+        favPet = pet[i].value;
+    }
 }
+console.log(favPet);
 
-console.log(buTton);
+for(let i = 0; i < hoBy.length; i++){
+    if(hoBy[i].checked){
+        hobbies.push(" "+ hoBy[i].value);
+    }
+}
+console.log(hobbies);
 
 console.log("nameInput", nameBox, "ageInput", nameAge);
+}
+
+// console.log(buTton);
+
+// console.log("nameInput", nameBox, "ageInput", nameAge);
